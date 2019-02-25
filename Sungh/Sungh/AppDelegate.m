127cb8a3efe31cb20eba960396bf796c6472218e
Sunsly <2524036812@qq.com>
2019-02-25 21:57:05 +0800

    Revert "no message"
    This reverts commit 47d115ae3687522bdb4d1bcf491887c228a79e8d.

--- Sungh/Sungh/AppDelegate.m
+++ Sungh/Sungh/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
 
+//    [AppDelegate ] kasndasdnalsdnlasdnasndkadsn
     NSString *a = @"hellowwwwwww";
     NSString *b = @"hellowwwwwww";
     
